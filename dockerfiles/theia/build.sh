8433776f0091ba76a1d50092b2ab8f14f998c908
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2021-05-07 02:00:36 +0530

    Update build.sh

--- dockerfiles/theia/build.sh
+++ dockerfiles/theia/build.sh
@@ -47,7 +47,9 @@ if [[ -z "$DOCKER_BUILD_TARGET" ]]; then
   if [ -n "${LABEL_CONTENT}" ]; then
     echo "Adding the CDN label..."
      if [ "$DOCKER_TARGET_PLATFORM" == "multiarch" ]; then
-        docker buildx build --push --platform linux/arm64,linux/amd64  --label che-plugin.cdn.artifacts="$(echo ${LABEL_CONTENT} | sed 's/ //g')" -t "${IMAGE_NAME}"
+        docker buildx build --push --platform linux/arm64,linux/amd64  --label che-plugin.cdn.artifacts="$(echo ${LABEL_CONTENT} | sed 's/ //g')" -t "${IMAGE_NAME}"-<<EOF
+FROM ${IMAGE_NAME}
+EOF
     else 
         docker build --label che-plugin.cdn.artifacts="$(echo ${LABEL_CONTENT} | sed 's/ //g')" -t "${IMAGE_NAME}-with-label" -<<EOF
 FROM ${IMAGE_NAME}
